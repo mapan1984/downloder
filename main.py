445bfe221873e7a0f728588268c0cf90646d7580
mapan <mapan1984@outlook.com>
2017-02-16 21:44:09 +0800

    use argparse processing parameters

--- main.py
+++ main.py
@@ -1,23 +1,24 @@
-import sys
 import os.path
+import argparse
 
 from config import DOWNLOAD_DIR
 from downloader import Downloader
 
 if __name__ == "__main__":
-    print(sys.argv)
-    if len(sys.argv) == 4:
-        argv = sys.argv
-        url = argv[1]
-        filename = argv[2]
-        thread_num = argv[3]
-        downloader = Downloader(url, os.path.join(DOWNLOAD_DIR, filename),
-                                thread_num=thread_num)
-        downloader.run()
+    parser = argparse.ArgumentParser()
+    parser.add_argument("url", type=str,
+                        help="get the sourse file url")
+    parser.add_argument("-f", "--file", type=str,
+                        help="point the download file name")
+    parser.add_argument("-t", "--thread", type=int, default=4,
+                        help="point downloader thread number")
+    args = parser.parse_args()
+    url = args.url
+    if args.file:
+        filename = args.file
     else:
-        url = input("url: ")
-        filename = input("filename: ")
-        thread_num = input("thread_num: ")
-        downloader = Downloader(url, os.path.join(DOWNLOAD_DIR, filename),
-                                thread_num=thread_num)
-        downloader.run()
+        filename = os.path.basename(url) or "download"
+    dest = os.path.join(DOWNLOAD_DIR, filename)
+    thread_num = args.thread
+    downloader = Downloader(url, dest, thread_num=thread_num)
+    downloader.run()
